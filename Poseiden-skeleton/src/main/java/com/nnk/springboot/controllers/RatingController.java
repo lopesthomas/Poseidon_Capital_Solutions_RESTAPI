b6f353181bac9d9b8fc149fa3efa223a39cca801
LOPES Thomas <thomaslopes001@gmail.com>
2025-05-16 22:48:10 +0200

    Enhance Rating functionality: inject RatingRepository, improve validation, and update UI templates to reflect changes in order field and user context

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -1,6 +1,12 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.repositories.RatingRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,11 +20,18 @@ import jakarta.validation.Valid;
 @Controller
 public class RatingController {
     // TODO: Inject Rating service
+    @Autowired
+    private RatingRepository ratingRepository;
 
     @RequestMapping("/rating/list")
     public String home(Model model)
     {
         // TODO: find all Rating, add to model
+        List<Rating> ratingList = ratingRepository.findAll();
+        model.addAttribute("ratings", ratingList);
+
+        String remoteUser = SecurityContextHolder.getContext().getAuthentication().getName();
+        model.addAttribute("remoteUser", remoteUser);
         return "rating/list";
     }
 
@@ -30,12 +43,23 @@ public class RatingController {
     @PostMapping("/rating/validate")
     public String validate(@Valid Rating rating, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return Rating list
-        return "rating/add";
+        if (result.hasFieldErrors()) {
+            model.addAttribute("rating", rating);
+            return "rating/add";
+        }
+        ratingRepository.save(rating);
+        return "redirect:/rating/list";
     }
 
     @GetMapping("/rating/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         // TODO: get Rating by Id and to model then show to the form
+        Rating rating = ratingRepository.findById(id).orElse(null);
+        if (rating != null) {
+            model.addAttribute("rating", rating);
+        } else {
+            model.addAttribute("error", "Rating not found");
+        }
         return "rating/update";
     }
 
@@ -43,12 +67,24 @@ public class RatingController {
     public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
                              BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Rating and return Rating list
+        if (result.hasFieldErrors()) {
+            model.addAttribute("rating", rating);
+            return "rating/update";
+        }
+        rating.setId(id);
+        ratingRepository.save(rating);
         return "redirect:/rating/list";
     }
 
     @GetMapping("/rating/delete/{id}")
     public String deleteRating(@PathVariable("id") Integer id, Model model) {
         // TODO: Find Rating by Id and delete the Rating, return to Rating list
+        Rating rating = ratingRepository.findById(id).orElse(null);
+        if (rating != null) {
+            ratingRepository.delete(rating);
+        } else {
+            model.addAttribute("error", "Rating not found");
+        }
         return "redirect:/rating/list";
     }
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -2,22 +2,39 @@ package com.nnk.springboot.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "rating")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Rating {
     // TODO: Map columns in data table RATING with corresponding java fields
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
     @Column(name = "moodysRating")
+    @NotBlank(message = "Moodys Rating is mandatory")
     String moodysRating;
     @Column(name = "sandPRating")
+    @NotBlank(message = "SandP Rating is mandatory")
     String sandPRating;
     @Column(name = "fitchRating")
+    @NotBlank(message = "Fitch Rating is mandatory")
     String fitchRating;
     @Column(name = "orderNumber")
-    Integer orderNumber;
+    @NotNull(message = "Order Number is mandatory")
+    Integer order;
 }
