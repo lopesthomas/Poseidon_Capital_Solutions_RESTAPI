0e818c5bfbc459ce5ad589e4670fc742582c7695
LOPES Thomas <thomaslopes001@gmail.com>
2025-05-17 13:06:51 +0200

    Refactor RatingTests: replace Assert with JUnit assertions, simplify Rating instantiation, and improve readability

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/RatingTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/RatingTests.java
@@ -1,18 +1,18 @@
 package com.nnk.springboot;
 
-import com.nnk.springboot.domain.Rating;
-import com.nnk.springboot.repositories.RatingRepository;
-
-import org.assertj.core.api.Assert;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Optional;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.repositories.RatingRepository;
+
 @SpringBootTest
 public class RatingTests {
 
@@ -21,26 +21,30 @@ public class RatingTests {
 
 	@Test
 	public void ratingTest() {
-		Rating rating = new Rating("Moodys Rating", "Sand PRating", "Fitch Rating", 10);
+		Rating rating = new Rating();
+		rating.setMoodysRating("Moodys Rating");
+		rating.setSandPRating("Sand PRating");
+		rating.setFitchRating("Fitch Rating");
+		rating.setOrder(10);
 
 		// Save
 		rating = ratingRepository.save(rating);
-		Assert.assertNotNull(rating.getId());
-		Assert.assertTrue(rating.getOrderNumber() == 10);
+		assertNotNull(rating.getId());
+		assertTrue(rating.getOrder() == 10);
 
 		// Update
-		rating.setOrderNumber(20);
+		rating.setOrder(20);
 		rating = ratingRepository.save(rating);
-		Assert.assertTrue(rating.getOrderNumber() == 20);
+		assertTrue(rating.getOrder() == 20);
 
 		// Find
 		List<Rating> listResult = ratingRepository.findAll();
-		Assert.assertTrue(listResult.size() > 0);
+		assertTrue(listResult.size() > 0);
 
 		// Delete
 		Integer id = rating.getId();
 		ratingRepository.delete(rating);
 		Optional<Rating> ratingList = ratingRepository.findById(id);
-		Assert.assertFalse(ratingList.isPresent());
+		assertTrue(ratingList.isEmpty());
 	}
 }
