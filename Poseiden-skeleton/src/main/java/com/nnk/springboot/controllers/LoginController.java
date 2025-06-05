4a14d2d1b6d3add111c26645cc583ee5e2aa0abb
LOPES Thomas <thomaslopes001@gmail.com>
2025-06-05 14:22:25 +0200

    Refactor pom.xml and LoginController: update project metadata and remove unused UserRepository dependency

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -1,12 +1,9 @@
 package com.nnk.springboot.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.nnk.springboot.repositories.UserRepository;
 
 /**
  * Controller for handling user login and authorization errors.
@@ -14,9 +11,6 @@ import com.nnk.springboot.repositories.UserRepository;
  */
 @Controller
 public class LoginController {
-
-    @Autowired
-    private UserRepository userRepository;
 
     /**
      * Displays the login page.
